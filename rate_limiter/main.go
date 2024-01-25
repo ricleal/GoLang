6d6a3203c4a55b9064f8273c888cd93182e61a80
Ricardo Leal <ricardo.leal@sezzle.com>
2024-01-25 08:54:10 -0500

    added rate limiter

---
+++ rate_limiter/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sync"
+	"time"
+)
+
+type RateLimiter struct {
+	maxMessages int
+	windowInSec int
+	mu          sync.Mutex
+
+	messageCount int
+	lastReset    time.Time
+}
+
+func NewRateLimiter(maxMessages int, windowInSec int) *RateLimiter {
+	return &RateLimiter{
+		maxMessages: maxMessages,
+		windowInSec: windowInSec,
+		lastReset:   time.Now(),
+	}
+}
+
+func (r *RateLimiter) Allow() (bool, time.Time) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	now := time.Now()
+
+	if r.messageCount < r.maxMessages {
+		r.messageCount++
+		return true, now
+	}
+	if now.Sub(r.lastReset) > time.Duration(r.windowInSec)*time.Second {
+		r.messageCount -= r.maxMessages
+		if r.messageCount < 0 {
+			r.messageCount = 0
+		}
+		r.lastReset = now
+	}
+	// c is the number of windows that have passed since the last reset
+	c := math.Ceil(float64(r.messageCount+1) / float64(r.maxMessages))
+
+	t := r.lastReset.Add(time.Duration(float64(r.windowInSec)*c)*time.Second - time.Duration(r.windowInSec)*time.Second)
+
+	r.messageCount++
+	return false, t
+}
+
+func main() {
+	// 100 messages per 10 seconds
+	limiter := NewRateLimiter(100, 10)
+
+	for i := 0; i <= 500; i++ {
+		allow, nextBatchTime := limiter.Allow()
+		fmt.Printf("Message %d (%v): %.0fs (%s)\n", i, allow, time.Until(nextBatchTime).Seconds(), nextBatchTime.Format("15:04:05"))
+	}
+}
