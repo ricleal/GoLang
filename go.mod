c043911340edc7661bbfeb442253ad4f916d2df2
Ricardo Leal <ricleal@gmail.com>
2024-09-14 15:45:33 -0400

    play with iterator

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module exp
 
-go 1.22.1
+go 1.23.0
 
 require (
 	github.com/bradfitz/gomemcache v0.0.0-20230905024940-24af94b03874
---
+++ iterator/exp1.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// see: https://antonz.org/go-1-23/
+
+type data struct {
+	key   string
+	value string
+}
+
+func expMap() {
+	var m sync.Map
+
+	m.Store("key1", data{"key1", "value1"})
+	m.Store("key2", data{"key2", "value2"})
+	m.Store("key3", data{"key3", "value3"})
+	m.Store("key4", data{"key4-inv", "value4"})
+	m.Store("key5", data{"key5", "value5"})
+
+	m.Range(func(key, value any) bool {
+		// cast key to string
+		if keyStr, ok := key.(string); ok {
+			// cast value to data
+			if valueData, ok := value.(data); ok {
+				if valueData.key == keyStr {
+					fmt.Printf("key: %s, value: %s\n", keyStr, valueData.value)
+				} else {
+					fmt.Println("❗key and value not match")
+				}
+			}
+		}
+		return true
+	})
+}
+
+func main() {
+	expMap()
+}
